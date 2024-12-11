2f0ebff6043dd3ee603d14f7ab31a7e8236caaad
LironeFitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-12-11 11:05:15 +0200

    Increase WebDriver wait time for QR code element to improve reliability in QR code generation

--- Python/app.py
+++ Python/app.py
@@ -204,7 +204,7 @@ class WhatsAppAutomation:
     def generate_qr_code(driver):
         """Save QR code screenshot, or capture full page if QR code is not found."""
         try:
-            WebDriverWait(driver, 15).until(
+            WebDriverWait(driver, 30).until(
                 EC.presence_of_element_located((By.XPATH, '//canvas[@aria-label="Scan this QR code to link a device!"]'))
             )
             qr_code_element = driver.find_element(By.XPATH, '//canvas[@aria-label="Scan this QR code to link a device!"]')
