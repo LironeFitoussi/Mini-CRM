b4e37f6ea2df1b5823b27b167619697dbbaecb8e
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-12-12 17:54:22 +0200

    Enable headless mode for Chrome in app_mac.py for improved automation

--- Python/app_mac.py
+++ Python/app_mac.py
@@ -156,7 +156,7 @@ def setup_driver():
         # chrome_options.add_argument("--profile-directory=Default")
         
         # Aggressive troubleshooting options
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
